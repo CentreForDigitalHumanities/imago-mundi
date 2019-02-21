3e9e7564318ab17db0983e35e2bed9ce51ba8ee4
R. Loeber <r.g.v.loeber@uu.nl>
2019-02-21 16:39:04 +0100

    Google maps integration, works somewhat. Variables not recognized as types yet, so must be passed as parameters.

--- frontend/src/imagomundi/search-view.ts
+++ frontend/src/imagomundi/search-view.ts
@@ -6,11 +6,19 @@ import searchTemplate from './search-template';
 import Collection from '../core/collection';
 import ImagoMundiCollection from './imagomundi-collection';
 import detailsTemplate from './details-template';
+//import mapTemplate from './map-template'; gaat niet werken, moet vanuit typescript zelf
+//import { createClient, GoogleMapsClient, google } from '@google/maps';
+// declare var google;
+// import { google } from "google-maps";
+// import { } from '@types/googlemaps'; //wordt niet herkend dit moet nog: Add typeroots to your  tsconfig.json
+
+
 
 export default class SearchView extends View {
     template = searchTemplate;
     resultTemplate = resultTemplate;
     detailsTemplate = detailsTemplate;
+    //mapTemplate = mapTemplate;
     initialSearchCollection: Collection;
     filterOptionsCollection = new ImagoMundiCollection;
     current_location_countries: String[] = [];
@@ -18,13 +26,108 @@ export default class SearchView extends View {
     table_keys: String[] = [];
     showdetails: boolean = false;
     id_last: Number;
+    // map: any; werkt niet om het global te maken, herkent de methods niet meer
+    // geocoder: any;
+
+    //googleMapsClient: GoogleMapsClient; dit werkt, maar is geen maps
+    //google: google;
+
+    //map:Google; //hier zou je hem moeten kunnen initialiseren als type
+    //mapRef: google.maps.Map;
 
     initialize() {
         this.listenTo(this.collection, 'update', this.update_searchresult); //als collection geupdated word,  method uitvoeren
+        //this.initMap();
+    }
+
+
+    //dit werkt. Probleem is nog dat de objecten niet door typescript als types worden herkend, dus ik kan ze enkel als parameters doorgeven. Hoe anders?
+    initMap() {
+        // var map = new google.maps.Map(document.getElementById("map"), { werkt ook
+        var map = new google.maps.Map($('#map').get(0), { //this is er van af, enkel dan herkent hij hem, waarschijnlijk omdat hij nu in de index staat. Todo: verplaatsen
+            zoom: 5,
+            center: { lat: 51.4, lng: 11.4 }
+        });
+
+        var geocoder = new google.maps.Geocoder();
+        this.geocodeAddress(geocoder, map);
+    }
+
+
+    geocodeAddress(geocoder, resultsMap) {
+
+        //var geocoder2 = new google.maps.Geocoder();
+        //console.log(geocoder2);
+        var addresses = [];
+        var geocode_max = 15;
+        var addresses_all = _.uniq(this.collection.pluck("address_current_location"));
+        addresses_all = remove(addresses_all, function (n) { return n != ""; });//remove empty
+
+        //Geocoding API: 50 requests per second (QPS), calculated as the sum of client-side and server-side queries.
+        //aantal adressen dus beperken.
+        //een max aantal is ook nog niet voldoende, schijnt dat het niet te snel achter elkaar mag
+        if (addresses_all.length < geocode_max) {
+            geocode_max = addresses_all.length;
+        }
+
+        for (i = 0; i < geocode_max; i++) {
+            addresses.push(addresses_all[i]);
+        };
+
+        console.log(addresses);
+
+
+        for (var i = 0; i < addresses.length; i++) {
+            //console.log(addresses[i]);
+
+            let address = addresses[i];// via let wordt address telkens een neiuwe variable, anders werd hij nooit vernieuwd als timeout werd gebruikt, dan kreeg je meteen de laatste in array
+
+
+            setTimeout(function () {
+
+                console.log('gaat dit geocoden:')
+                console.log(address)
+                geocoder.geocode({ 'address': address }, function (results, status) {
+
+                    if (status === 'OK') {
+                        resultsMap.setCenter(results[0].geometry.location);
+                        var marker = new google.maps.Marker({
+                            map: resultsMap,
+                            position: results[0].geometry.location,
+                            title: results[0].formatted_address
+
+                        });
+
+                        console.log('resultaat:')
+                        console.log(results)
+
+                        var infowindow = new google.maps.InfoWindow({
+                            content: results[0].formatted_address //todo, ook de titel en shelfmark meegeven, hoe als er meerder manuscripten zijn?
+                        });
+
+                        //infowindow.open(map, marker);
+                        google.maps.event.addListener(marker, 'click', (function (marker, i) {
+                            return function () {
+                                infowindow.open(map, marker);
+                            }
+                        })(marker, i));
+
+                    } else {
+                        console.log('Geocode was not successful for the following reason: ' + status);
+                    }
+                }
+
+                );
+
+            }, 200);
+
+        }//for
 
     }
 
-    search(event) { //waar komt even param vandaan?
+
+
+    search(event) {
         var self = this; // pass the class this as self, because jquery has its own this
         event.preventDefault();
         this.collection.fetch({
@@ -34,9 +137,7 @@ export default class SearchView extends View {
                 self.initialSearchCollection = new Collection(collection.models);// dit is nodig omdat in javascript by reference wordt doorgegeven
                 // je kan dus niet zeggen self.initialSearchCollection=collection, dan is dat nl ook een referentie, 
                 // en verandert hij mee met de waarde van de collection
-
-                self.applyFilters();// geeft "this is undefined" Searchview.filter() werkt ook niet, via self =this lukt het wel
-
+                self.applyFilters();
             },
             error: function (collection, response, options) {
                 console.log("error");
@@ -49,6 +150,8 @@ export default class SearchView extends View {
         this.collection.set(this.initialSearchCollection.models);//first reset to original collection
         this.filterCountryLanguage();
         this.filterDates();
+        //this.prepareAddresses();
+        this.initMap();
         return this.collection.toJSON();
     }
 
@@ -91,37 +194,60 @@ export default class SearchView extends View {
     //just once called from imagomundi.ts, get collection to create dynamically the filter select options, and append the template
     render() {
         var self = this;
+
         this.filterOptionsCollection.fetch({
             success: function (filterOptionsCollection, response, options) {
                 //filter unique on language and countrylocation 
-                self.current_location_countries = _.uniq(filterOptionsCollection.pluck("current_location_country"));//select attribute and keep unique
-                self.current_location_countries = remove(self.current_location_countries, function (n) { return n != ""; });//remove empty
-                self.current_location_countries.sort();
 
-                self.languages = _.uniq(filterOptionsCollection.pluck("language"));
-                self.languages = remove(self.languages, function (n) { return n != ""; });//remove empty
-                self.languages.sort();
+                self.prepareCountriesFilter();
+                self.prepareLanguagesFilter();
                 //self.table_keys = self.filterOptionsCollection.at(1).attributes;
 
                 self.$el.html(self.template({
                     countries_select: self.current_location_countries,
                     languages_select: self.languages,
+                    addresses: ['address1', 'addres2'],
                     //table_keys: self.table_keys //werkt, maar tabel veel te breed, moet onder elkaar
                 }));
+
+                //self.initMap();
 
             },
             error: function (collection, response, options) {
                 console.log("error");
             }
+
+
         });
 
         return this;
     }
 
+    prepareCountriesFilter() {
+        this.current_location_countries = _.uniq(this.filterOptionsCollection.pluck("current_location_country"));//select attribute and keep unique
+        this.current_location_countries = remove(this.current_location_countries, function (n) { return n != ""; });//remove empty
+        this.current_location_countries.sort();
+    }
+
+    prepareLanguagesFilter() {
+        this.languages = _.uniq(this.filterOptionsCollection.pluck("language"));
+        this.languages = remove(this.languages, function (n) { return n != ""; });//remove empty
+        this.languages.sort();
+    }
+
+    //get unique adresses en put them in array for google map
+    // prepareAddresses() {
+    //     var addresses = _.uniq(this.collection.pluck("address_current_location"));
+    //     addresses = remove(addresses, function (n) { return n != ""; });//remove empty
+
+    //     console.log(addresses)
+    // }
+
     update_searchresult() {
         this.$('#searchresult').html(this.resultTemplate({ results: this.collection.toJSON() }));
         this.$('#countresult').html("N = " + this.collection.toJSON().length);
         this.$('#titlebox').hide();//hide title to have more room for table
+
     }
 
 
@@ -134,7 +260,7 @@ export default class SearchView extends View {
             var detailsmodel = JSON.parse(JSON.stringify(this.collection.where({ id: id })));//json.parse turns json in an object
             this.$('#showdetails').html(this.detailsTemplate({ detailsmodel: detailsmodel[0] }));//object is multidimensional, so go one layer deeper
 
-            console.log(detailsmodel[0]);
+            //console.log(detailsmodel[0]);
         }
         else {
             this.closeDetails(event);
@@ -148,9 +274,9 @@ export default class SearchView extends View {
         if (event.target.className != '') {
             this.$('#showdetails').empty();
             this.showdetails = false;
-            console.log('close details')
+            //console.log('close details')
         }
-        console.log(event.target.className);
+        //console.log(event.target.className);
     }
 
 
@@ -164,6 +290,18 @@ export default class SearchView extends View {
         event.preventDefault();
         this.$('.modal').removeClass("is-active");
     }
+
+    resetFilters(event) {
+        event.preventDefault();
+        $('#language').prop('selectedIndex', 0);
+        $('#current_location_country').prop('selectedIndex', 0);
+        $('#date_from').prop('selectedIndex', 0);
+        $('#date_until').prop('selectedIndex', 0);
+        this.applyFilters();
+        //console.log('reset button')
+    }
+
+
 
 
 }
@@ -182,6 +320,11 @@ extend(SearchView.prototype, {
         'click #help_button': 'showHelp',
         'click #close_button': 'closeHelp',
         'click .hero': 'closeDetails',
-        //'click .modal-imagomundi': 'showDetails', helpt niet
+        'click .title': 'closeDetails',
+        'click .modal-background': 'closeHelp',
+        'click #reset_button': 'resetFilters',
+
+
+
     },
 });
