b399292dac662c9961cddde41e58da5233021392
R. Loeber <r.g.v.loeber@uu.nl>
2019-03-20 16:02:54 +0100

    api url dynamic in collection

--- frontend/src/imagomundi/imagomundi-collection.ts
+++ frontend/src/imagomundi/imagomundi-collection.ts
@@ -1,11 +1,12 @@
 import { extend } from 'lodash';
 import Collection from '../core/collection';
 import ImagoMundiModel from './imagomundi-model'
+import { apiRoot } from 'config.json';
 
 export default class ImagoMundiCollection extends Collection {
 }
 
 extend(ImagoMundiCollection.prototype, {
-    url: 'http://localhost:8000/api/imagomundi/',
+    url: apiRoot + 'imagomundi/',
     model: ImagoMundiModel,
 });
