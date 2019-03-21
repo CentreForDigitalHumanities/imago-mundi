24958822e01651062d7ed14947e5e6669fc917ef
R. Loeber <r.g.v.loeber@uu.nl>
2019-03-21 12:23:52 +0100

    Template force override does not work, as override happens after the import/export module adds teh import/exp buttons, so they are lost. Error found in the template itself (unknown variable). So the change_list.html copied from the original on the server, and the geo button added. This should work, I assume, as the overriding template is now exactly the same as the original.

--- backend/imago_mundi_app/admin.py
+++ backend/imago_mundi_app/admin.py
@@ -11,8 +11,7 @@ from django import template
 
 
 class ImagoMundiResource(resources.ModelResource):
-    # attribute is de naam in het model, Columnname is van de import
-    # gebruikt nu dus eigenlijk de method 'fields' van import_export class?
+    # attribute is name in model, Columnname is the one of import
     shelfmark = Field(column_name='Shelfmark', attribute="shelfmark")
     date_from = Field(column_name='date_from', attribute="date_from")
     date_until = Field(column_name='date_until', attribute="date_until")
@@ -20,20 +19,14 @@ class ImagoMundiResource(resources.ModelResource):
         column_name='current_location_country', attribute="current_location_country")
     current_location_town = Field(
         column_name='current_location_town', attribute="current_location_town")
-
-    # 7 maart
     place_of_origin_country = Field(column_name='place_of_origin_country',
                                     attribute="place_of_origin_country")
     place_of_origin_town = Field(column_name='place_of_origin_town',
                                  attribute="place_of_origin_town")
-
-    # nieuw
     address_current_location = Field(
         column_name='Address current location', attribute="address_current_location")
-
     place_of_origin = Field(column_name='Place of origin',
                             attribute="place_of_origin")
-
     owner_and_location_1000_1100 = Field(
         column_name='Owner and location 1000-1100', attribute="owner_and_location_1000_1100")
     owner_and_location_1100_1200 = Field(
@@ -64,11 +57,8 @@ class ImagoMundiResource(resources.ModelResource):
     attribution_of_author = Field(
         column_name='Attribution of author', attribute="attribution_of_author")
     decorations = Field(column_name='Decorations', attribute="decorations")
-
-    # nieuw
     images = Field(
         column_name='Images', attribute="images")
-
     manuscript_content = Field(
         column_name='Manuscript content', attribute="manuscript_content")
     bibliography = Field(column_name='Bibliography', attribute="bibliography")
@@ -76,7 +66,6 @@ class ImagoMundiResource(resources.ModelResource):
 
     class Meta:  # for adding attributes
         model = ImagoMundi
-        # fields = ['shelfmark', 'date_from', 'current_location_country']
         exclude = ('id', 'created_date')
         to_encoding = 'utf-8'  # export data encoding
         from_encoding = 'utf-8'  # import data encoding
@@ -95,14 +84,12 @@ class ImagoMundiResource(resources.ModelResource):
         dataset.insert_col(4, col=["", ]*dataset.height,
                            header="current_location_town")
 
-        # 7maart
         dataset.insert_col(5, col=["", ]*dataset.height,
                            header="place_of_origin_country")
         dataset.insert_col(6, col=["", ]*dataset.height,
                            header="place_of_origin_town")
 
     # override this method, to enable extra row headers
-
     def get_instance(self, instance_loader, row):
         return False
 
@@ -137,7 +124,7 @@ class ImagoMundiResource(resources.ModelResource):
         else:
             current_location_town = ""
 
-        # 7 maart split place of origin in two columns. In before_import method already there are these extra columns inserted
+        # split place of origin in two columns. In before_import method already there are these extra columns inserted
         place_of_origin_split = row['Place of origin'].split(",")
         if len(place_of_origin_split) > 1:
             place_of_origin_town = place_of_origin_split[1]
@@ -146,69 +133,11 @@ class ImagoMundiResource(resources.ModelResource):
 
         row.update({'date_from': date_split[0], 'date_until': date_until,
                     'current_location_country': location_split[0], 'current_location_town': current_location_town,
-                    # 7maart
                     'place_of_origin_country': place_of_origin_split[0], 'place_of_origin_town': place_of_origin_town
                     })
-
-        # print(row)
-
-
-# doet zelfde als admin.site.register
 
 
 @admin.register(ImagoMundi)
 class ImagoMundiAdmin(ImportExportModelAdmin):
-
+    # change_list_template = "../templates/admin/change_list.html" # dit werkt, maar je bent import export buttons kwijt, was omdat hij pas in tweede instantie de override doet
     resource_class = ImagoMundiResource
-    # override list template , with one that has a button to geocode. This triggers code in views.py
-    change_list_template = "../templates/admin/change_list.html"
-
-    # meest duidelijk
-    # http://books.agiliq.com/projects/django-admin-cookbook/en/latest/action_buttons.html
-
-    # def get_urls(self):
-    #     from django.conf.urls import url
-    #     urls = super(ImagoMundiAdmin, self).get_urls()
-    #     urls += [
-    #         # was path in voorbeeld, is oud?
-    #         # hij herkent dit blijkbaar niet. Of moet er toch iets bij urls.py?
-    #         url(r'^geocode/$', self.geocode_current_address),
-    #     ]
-    #     return urls
-
-    # def geocode_current_address(self, request):
-    #     print(request)
-    #     print('test')
-    #     self.message_user(request, "geocoded")
-    #     return HttpResponseRedirect("../")
-
-    # actions = ['geocode_current_address']
-
-    # def geocode_current_address(self, request, queryset):
-    #     print(request)
-    #     print('test')
-
-
-# deze aanroepen via form met button
-
-#actions = ['geocode_current_address']
-
-# dit elimineert were de import functie. du via de class ImagoMundiAdmin?
-
-
-# class GeocodeAdmin(admin.ModelAdmin):
-#     # in form: <input type="hidden" name="action" value="geocode_current_address" />
-#     actions = ['geocode_current_address']
-
-#     def geocode_current_address(self, request, queryset):
-#         print(request)
-#         print('test')
-    # of doen zaosl met normale views
-    # https://developer.mozilla.org/en-US/docs/Learn/Server-side/Django/Forms
-
-    # meest duidelijk
-    # http://books.agiliq.com/projects/django-admin-cookbook/en/latest/action_buttons.html
-
-
-# admin.site.unregister(ImagoMundi)
-#admin.site.register(ImagoMundi, GeocodeAdmin)
