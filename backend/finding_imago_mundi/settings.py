a3d5c9f1b660074909ef55c0683541510c449a03
Xander Vertegaal <a.j.j.vertegaal@uu.nl>
2025-01-24 17:23:32 +0100

    Upgrade to Django 5

--- backend/finding_imago_mundi/settings.py
+++ backend/finding_imago_mundi/settings.py
@@ -86,6 +86,8 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'finding_imago_mundi.wsgi.application'
 
+DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
+
 
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
