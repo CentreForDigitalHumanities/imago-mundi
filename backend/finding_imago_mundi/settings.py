57e96777ece63c2b885d23f1a273e89d37535787
Xander Vertegaal <a.j.j.vertegaal@uu.nl>
2025-02-04 11:57:29 +0100

    Add common_settings

--- backend/finding_imago_mundi/settings.py
+++ backend/finding_imago_mundi/settings.py
@@ -40,32 +40,6 @@ CSRF_COOKIE_HTTPONLY = True
 
 GMAPS_APIKEY = os.environ.get('GMAPS_APIKEY', 'none')
 
-# Application definition
-
-INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'livereload',
-    'django.contrib.staticfiles',
-    'rest_framework',
-    'import_export',
-    'imago_mundi_app',
-]
-
-MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'livereload.middleware.LiveReloadScript',
-]
-
 ROOT_URLCONF = 'finding_imago_mundi.urls'
 
 TEMPLATES = [
